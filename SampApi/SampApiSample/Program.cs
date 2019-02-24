da7d04bf48315479dab15b5a53a391ccd46a1040
Kfir Eichenblat <kfirprods@gmail.com>
2019-02-24 21:47:49 +0200

    Initial release

---
+++ SampApi/SampApiSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SampApiSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampApi.SampApi.AddMessageToChat("{FFFFFF}SAMP {FF0000}API {00FF00}sample");
+
+            SampApi.SampApi.AddMessageToChat(
+                $"You are in {SampApi.SampApi.GetPlayerCurrentZone()} with facing angle {SampApi.SampApi.GetPlayerFacingAngle():F1}");
+
+            Console.ReadLine();
+        }
+    }
+}
